d1ae542a4cc757330c2816cef3ff00eba831b5cd
Nagabhushanam <nagabhushanamn@gmail.com>
2020-05-21 15:18:52 +0530

    Nag component

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-/**
- * 
- * this is App component
- * 
- */
+import Nag from './components/Nag'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <h1>learn devops!</h1>
+    <div className="container">
+      <hr />
+      <h1>INTCHN20IN001</h1>
+      <hr />
+      <Nag />
+
     </div>
   );
 }
---
+++ ui/src/components/Nag.js
@@ -0,0 +1,17 @@
+
+
+import React from 'react';
+
+const Nag = () => {
+    return (
+        <div>
+            <pre>
+                Name        : Nag,
+                Profession  : trainer,
+                Address     : chennai
+           </pre>
+        </div>
+    );
+};
+
+export default Nag;
